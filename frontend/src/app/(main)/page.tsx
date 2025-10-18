9c891925082f42c9e5112c34b4cf366449e51c5e
nrysk <88011751+nrysk@users.noreply.github.com>
2025-10-19 01:13:55 +0900

    feat: EggViewerに機能を追加

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
       </div>
       <EggViewer
         crackUrls={homePageData.egg.cracks.map((c) => c.transparent_url)}
-        className="h-80 w-80 lg:h-96 lg:w-96"
+        className="h-80 w-80 overflow-clip rounded-2xl lg:h-96 lg:w-96"
       />
 
       <Progress
--- frontend/src/components/egg-viewer.tsx
+++ frontend/src/components/egg-viewer.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { Decal, OrbitControls, useGLTF, useTexture } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
 
 const EGG_MODEL_PATH = "/models/egg.glb";
@@ -9,14 +11,12 @@ const CRACK_POSITIONS = Array.from(
   { length: MAX_CRACKS },
   (_, i) =>
     new THREE.Vector3(
-      Math.sin((i / MAX_CRACKS) * Math.PI * 2),
-      Math.cos((i / MAX_CRACKS) * Math.PI * 2),
-      Math.sin((i / MAX_CRACKS) * Math.PI * 2 * 1.5),
+      Math.sin((i / MAX_CRACKS) * Math.PI * 4) * 0.8,
+      Math.sin((i / MAX_CRACKS) * Math.PI * 6) * 0.7,
+      Math.cos((i / MAX_CRACKS) * Math.PI * 4) * 0.8,
     ),
 );
-const CRACK_ROTATIONS = CRACK_POSITIONS.map((pos) =>
-  new THREE.Euler().setFromVector3(pos.clone().multiplyScalar(Math.PI / 2)),
-);
+const CRACK_ROTATIONS = CRACK_POSITIONS.map((pos) => CalcRotation(pos));
 
 interface EggViewerProps {
   crackUrls: string[];
@@ -25,22 +25,43 @@ interface EggViewerProps {
 
 export default function EggViewer({ crackUrls, className }: EggViewerProps) {
   return (
-    <Canvas className={className}>
-      <color attach="background" args={["#f0f0f0"]} />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
-      <OrbitControls enableZoom={true} />
-      <EggModelWithDecals crackUrls={crackUrls} />
-    </Canvas>
+    <div className={cn("h-full w-full", className)}>
+      <Canvas camera={{ position: [0, 1, 2] }}>
+        <color attach="background" args={["#f0f0f0"]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={0.7} />
+        <OrbitControls
+          enableZoom={true}
+          minPolarAngle={Math.PI / 3}
+          maxPolarAngle={(Math.PI * 2) / 3}
+        />
+        <EggModelWithDecals crackUrls={crackUrls} />
+      </Canvas>
+    </div>
   );
 }
 
 function EggModelWithDecals({ crackUrls }: { crackUrls: string[] }) {
   const { nodes, materials } = useGLTF(EGG_MODEL_PATH);
   const textures = useTexture(crackUrls);
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  // ヒビの数に応じて揺れるアニメーション
+  const crackCount = textures.length;
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime();
+    if (crackCount === 0 || time % 6 < 4) {
+      return;
+    } else if (crackCount < MAX_CRACKS / 2) {
+      meshRef.current!.rotation.z = 0.1 * Math.sin(time * Math.PI * 1);
+    } else {
+      meshRef.current!.rotation.z = 0.2 * Math.sin(time * Math.PI * 1.5);
+    }
+  });
 
   return (
     <mesh
+      ref={meshRef}
       geometry={(nodes.Egg as THREE.Mesh).geometry}
       material={materials.EggMaterial}
     >
@@ -50,9 +71,16 @@ function EggModelWithDecals({ crackUrls }: { crackUrls: string[] }) {
           position={CRACK_POSITIONS[index]}
           rotation={CRACK_ROTATIONS[index]}
           map={texture}
-          scale={0.5}
+          scale={0.7}
         />
       ))}
     </mesh>
   );
+}
+
+function CalcRotation(position: THREE.Vector3) {
+  const dummy = new THREE.Object3D();
+  dummy.position.copy(position);
+  dummy.lookAt(new THREE.Vector3(0, 0, 0));
+  return dummy.rotation;
 }
